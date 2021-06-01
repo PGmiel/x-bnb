2c8b83c52b52efa0e218e127d39851d901084206
Kyrst1 <kyrstinlegrand@hotmail.fr>
2021-06-01 18:10:33 +0200

    schemas

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Review.destroy_all
 CategoryActivity.destroy_all
 Category.destroy_all
 Activity.destroy_all
