3a1e12a4a50023eb714aa803d59ec6c325471f04
Roch de Vidts <roch-dv@hotmail.com>
2021-06-02 17:09:53 +0200

    debug map on heroku

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -10,6 +10,12 @@ class ActivitiesController < ApplicationController
     else
       @activities = Activity.all
     end
+     @markers = @activities.geocoded.map do |activity|
+      {
+        lat: activity.latitude,
+        lng: activity.longitude
+      }
+      end
   end
 
   def show
