75e9f986e36280ca36a1d39ec28dc71a64124555
Pierre-Guillaume <80978648+PGmiel@users.noreply.github.com>
2021-06-03 12:24:45 +0200

    hustled on the filter, starts to work

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -4,18 +4,14 @@ class ActivitiesController < ApplicationController
   skip_before_action :authenticate_user!, only: :show
 
   def index
-    if params[:category]
-      # @activities = Activity.where(category_id: params[:category])
-      @activities = CategoryActivity.where(category_id: params[:category]).map{ |category_activity| category_activity.activity }
-    else
-      @activities = Activity.all
-    end
-     @markers = @activities.geocoded.map do |activity|
+    @activities = Activity.all
+
+    @markers = @activities.geocoded.map do |activity|
       {
         lat: activity.latitude,
         lng: activity.longitude
       }
-      end
+    end
   end
 
   def show
